93415e958cf0ab6a4a1435f332ae62ef8a98e642
Dushime <cleonidushime@gmail.com>
2025-02-24 16:28:48 +0100

    init commit c2

--- src/Main.java
+++ src/Main.java
@@ -5,15 +5,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-
-
-
         Scanner sc = new Scanner(System.in);
         System.out.println("What is your name?");
         String a= sc.nextLine();
-        System.out.println("Hello, " + a + "!");
 
 
+        System.out.println("What year were you born in?");
+        int b= sc.nextInt();
+        int age= 2025-b;
+        System.out.println("Hello, " + a + "!"+ "\nYou will turn "+ age +" this year" );
 
         }
     }
