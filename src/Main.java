6fe93992981afa6554e61614036171de476c22b1
Dushime <cleonidushime@gmail.com>
2025-02-24 16:14:26 +0100

    init commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,19 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello World");
+
+
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("What is your name?");
+        String a= sc.nextLine();
+        System.out.println("Hello, " + a + "!");
+
+
 
         }
     }
